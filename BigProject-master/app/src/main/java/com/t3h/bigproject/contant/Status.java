d8a9552abbd35d78c32c0f05245333d96cef3b92
TrinhMinhTam <minhtamtrinh26@gmail.com>
2020-02-07 10:52:22 +0700

    update

---
+++ BigProject-master/app/src/main/java/com/t3h/bigproject/contant/Status.java
@@ -0,0 +1,8 @@
+package com.t3h.bigproject.contant;
+
+public class Status {
+	public static int USER = 1;
+	public static int PRODUCT = 2;
+	public static int CATEGORY = 3;
+	public static int HIDE = 4;
+}
--- BigProject-master/app/src/main/java/com/t3h/bigproject/fragment/screen/drinkmenu/api/DrinkMenuApiService.java
+++ BigProject-master/app/src/main/java/com/t3h/bigproject/fragment/screen/drinkmenu/api/DrinkMenuApiService.java
@@ -1,6 +1,7 @@
 package com.t3h.bigproject.fragment.screen.drinkmenu.api;
 
 import com.t3h.bigproject.contant.BaseUrl;
+import com.t3h.bigproject.model.Image;
 import com.t3h.bigproject.model.Product;
 
 import java.util.List;
@@ -12,35 +13,48 @@ import retrofit2.Response;
 public class DrinkMenuApiService {
 	private DrinkApiBuilder drinkApiBuilder;
 
-	public interface serviceCallBack{
+	public interface ServiceCallBack{
 		void success(String code,Object respone);
 		void fail(String code,String message);
 	}
 
-	public <D> void checkSuccess(Response<DrinkMenuRespone<D>> response,serviceCallBack serviceCallBack){
+	public <D> void checkSuccess(Response<DrinkMenuRespone<D>> response,ServiceCallBack ServiceCallBack){
 		if (response.isSuccessful()&&response.body()!=null){
 			if (response.body().getCode() !=null && response.body().getCode().equalsIgnoreCase("200")){
-				serviceCallBack.success(response.code()+"",response.body().getData());
+				ServiceCallBack.success(response.code()+"",response.body().getData());
 			}
 		}else {
-			serviceCallBack.fail(response.code()+"",response.body().getMessage());
+			ServiceCallBack.fail(response.code()+"",response.body().getMessage());
 		}
 	}
 
-	public <D> void checkFail(serviceCallBack serviceCallBack,Throwable t){
-		serviceCallBack.fail("404",t.getMessage());
+	public <D> void checkFail(ServiceCallBack ServiceCallBack,Throwable t){
+		ServiceCallBack.fail("404",t.getMessage());
 	}
 
-	public void drinkMenu(final serviceCallBack serviceCallBack){
+	public void getImage(final ServiceCallBack ServiceCallBack,int otherId,int idStatus){
+		drinkApiBuilder.getInstance(BaseUrl.BASE_URL).getImage(otherId,idStatus).enqueue(new Callback<DrinkMenuRespone<List<Image>>>() {
+			@Override
+			public void onResponse(Call<DrinkMenuRespone<List<Image>>> call, Response<DrinkMenuRespone<List<Image>>> response) {
+				checkSuccess(response,ServiceCallBack);
+			}
+
+			@Override
+			public void onFailure(Call<DrinkMenuRespone<List<Image>>> call, Throwable t) {
+				checkFail(ServiceCallBack,t);
+			}
+		});
+	}
+	public void drinkMenu(final ServiceCallBack ServiceCallBack){
 		drinkApiBuilder.getInstance(BaseUrl.BASE_URL).getDrinkMenu().enqueue(new Callback<DrinkMenuRespone<List<Product>>>() {
 			@Override
 			public void onResponse(Call<DrinkMenuRespone<List<Product>>> call, Response<DrinkMenuRespone<List<Product>>> response) {
-				checkSuccess(response,serviceCallBack);
+				checkSuccess(response,ServiceCallBack);
 			}
 
 			@Override
 			public void onFailure(Call<DrinkMenuRespone<List<Product>>> call, Throwable t) {
-				checkFail(serviceCallBack,t);
+				checkFail(ServiceCallBack,t);
 			}
 		});
 	}
